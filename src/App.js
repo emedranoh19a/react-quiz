30dc3b23177d443e98952cced42365aa0e18205e
user <user@usernoMacBook-Pro.local>
2023-11-17 13:34:40 +0900

    Fake API, and setup of useReducer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
+import Header from "./Header";
+import Main from "./Main";
+import { useEffect, useReducer } from "react";
+
+//state: loading, error, ready(to start the quiz), active, finished
+const initialState = { questions: [], status: "loading" };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+
+    default:
+      throw new Error("action unknown");
+  }
+}
+
 function App() {
-  return <h1>React Quiz</h1>;
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch(`http://localhost:8000/questions`)
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({ type: "dataReceived", data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question</p>
+      </Main>
+    </div>
+  );
 }
 
 export default App;
